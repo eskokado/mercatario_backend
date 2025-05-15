e247b3cf86640fdf81fa85d17feb6c758ee7d313
eskokado <eskokado@gmail.com>
2025-05-14 21:35:20 -0300

    fix: relations documentos_pessoais

--- app/models/credor.rb
+++ app/models/credor.rb
@@ -2,7 +2,7 @@ class Credor < ApplicationRecord
   self.table_name = "credores"
 
   has_many :precatorios, foreign_key: "credor_id"
-  has_many :documentos_pessoals, foreign_key: "credor_id"
+  has_many :documentos_pessoais, foreign_key: "credor_id"
   has_many :certidoes, foreign_key: "credor_id"
   validates :cpf_cnpj, uniqueness: true
 end
