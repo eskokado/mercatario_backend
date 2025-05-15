d8df2e42468b6e68c9096d2554bc8c4fd77bcfb9
eskokado <eskokado@gmail.com>
2025-05-15 16:56:17 -0300

    fix: create relations credor

--- app/models/precatorio.rb
+++ app/models/precatorio.rb
@@ -1,5 +1,13 @@
 class Precatorio < ApplicationRecord
   self.table_name = "precatorios"
 
-  belongs_to :credor, foreign_key: "credor_id"
+  belongs_to :credor, foreign_key: "credor_id", inverse_of: :precatorios
+
+  before_validation :set_credor_id, if: -> { credor.present? }
+
+  private
+
+  def set_credor_id
+    self.credor_id ||= credor.id
+  end
 end
