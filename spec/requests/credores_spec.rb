89c60470416872c6c2251e335f67a9bb080ddf72
eskokado <eskokado@gmail.com>
2025-05-15 17:49:09 -0300

    fix: update swagger get credor

--- spec/requests/credores_spec.rb
+++ spec/requests/credores_spec.rb
@@ -61,16 +61,24 @@ RSpec.describe 'Credores', type: :request do
       parameter name: :id, in: :path, type: :integer
 
       response '200', 'Credor encontrado' do
-        schema type: :object,
-          properties: {
-            id: { type: :integer },
-            nome: { type: :string },
-            precatorios: { type: :array },
-            documentos_pessoais: { type: :array },
-            certidoes: { type: :array }
-          }
-
-        let(:id) { Credor.create(nome: 'Teste', cpf_cnpj: '123').id }
+        example 'application/json', :success, {
+          id: 1,
+          nome: 'João Silva',
+          cpf_cnpj: '123.456.789-09',
+          email: 'example@email.com',
+          telefone: '12345678901',
+          created_at: '2025-05-15T20:24:24.773Z',
+          updated_at: '2025-05-15T20:24:24.773Z',
+          precatorios: [
+            {
+              id: 2,
+              numero_precatorio: 'numero002',
+              valor_nominal: '200.0',
+              foro: 'foro tal',
+              data_publicacao: '2025-05-15'
+            }
+          ]
+        }
         run_test!
       end
     end
