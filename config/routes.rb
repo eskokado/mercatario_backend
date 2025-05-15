0542a1e4acab04d27bf68b342fec750b2d1be07e
eskokado <eskokado@gmail.com>
2025-05-14 21:36:48 -0300

    feat: new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+  resources :credores do
+    member do
+      post "documentos", to: "documentos_pessoais#upload"
+      post "certidoes", to: "certidoes#upload_manual"
+      post "buscar-certidoes", to: "certidoes#buscar_api"
+    end
+  end
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  namespace :api do
+    get "certidoes", to: "certidoes#consultar"
+  end
 end
