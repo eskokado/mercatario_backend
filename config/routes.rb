a92dcf0077d2537d712c26956237e16edca836a5
eskokado <eskokado@gmail.com>
2025-05-15 17:25:57 -0300

    feat: routes api-docs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
+  mount Rswag::Ui::Engine => "/api-docs"
+  mount Rswag::Api::Engine => "/api-docs"
   resources :credores do
     member do
       post "documentos", to: "documentos_pessoais#upload"
