8a5e437607a2b3b9d6b70846bc02238d3b9d42b5
eskokado <eskokado@gmail.com>
2025-05-16 13:51:13 -0300

    feat: certidoes controller

---
+++ app/controllers/certidoes_controller.rb
@@ -0,0 +1,35 @@
+class CertidoesController < ApplicationController
+  before_action :set_credor
+
+  def upload_manual
+    certidao = @credor.certidoes.new(tipo: params[:tipo], origem: params[:origem], status: params[:status])
+
+    if params[:arquivo].present?
+      certidao.arquivo.attach(params[:arquivo])
+
+      if certidao.save
+        arquivo_url = rails_blob_url(certidao.arquivo, host: request.base_url)
+        certidao.update_columns(arquivo_url: arquivo_url, recebida_em: Time.current)
+
+        render json: { success: true, message: "Certidão salvo com sucesso" }, status: :ok
+      else
+        render json: { errors: certidao.errors }, status: :unprocessable_entity
+      end
+    else
+      render json: { errors: { arquivo: [ "não pode ficar em branco" ] } }, status: :unprocessable_entity
+    end
+  end
+
+  def buscar_api
+    certidoes = @credor.certidoes.order(recebida_em: :desc)
+    render json: certidoes
+  end
+
+  private
+
+  def set_credor
+    @credor = Credor.find(params[:credor_id] || params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Credor não encontrado" }, status: :not_found
+  end
+end
