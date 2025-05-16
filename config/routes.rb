58bd3654ce1b50085051c392d56cf063e9d984f1
eskokado <eskokado@gmail.com>
2025-05-16 13:47:06 -0300

    fix: limit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => "/api-docs"
   mount Rswag::Api::Engine => "/api-docs"
-  resources :credores do
+  resources :credores, only: [ :show, :create ] do
     member do
       post "documentos", to: "documentos_pessoais#upload"
       post "certidoes", to: "certidoes#upload_manual"
