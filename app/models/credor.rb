9f55d80429e9165ee2b3729cf653b906b94ee497
eskokado <eskokado@gmail.com>
2025-05-16 13:48:31 -0300

    fix: add validates credor

--- app/models/credor.rb
+++ app/models/credor.rb
@@ -17,7 +17,11 @@ class Credor < ApplicationRecord
   has_many :precatorios, foreign_key: "credor_id", inverse_of: :credor
   has_many :documentos_pessoais, foreign_key: "credor_id"
   has_many :certidoes, foreign_key: "credor_id"
+
+  validates :nome, presence: true
   validates :cpf_cnpj, uniqueness: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :telefone, presence: true
 
   accepts_nested_attributes_for :precatorios
 
@@ -26,6 +30,12 @@ class Credor < ApplicationRecord
       include: {
         precatorios: {
             only: [ :id, :numero_precatorio, :valor_nominal, :foro, :data_publicacao ]
+        },
+        documentos_pessoais: {
+            only: [ :id, :tipo, :arquivo_url, :enviado_em ]
+        },
+        certidoes: {
+            only: [ :id, :tipo, :origem, :status, :arquivo_url, :recebida_em ]
         }
       }
     ))
