2d43baaebd4843f2128d3c863fcd1098cdc494dc
eskokado <eskokado@gmail.com>
2025-05-17 10:12:41 -0300

    fix: add validation file

--- app/models/certidao.rb
+++ app/models/certidao.rb
@@ -27,4 +27,13 @@ class Certidao < ApplicationRecord
   validates :tipo, presence: true, inclusion: { in: TIPOS_CERTIDOES }
   validates :origem, presence: true, inclusion: { in: ORIGENS }
   validates :status, presence: true, inclusion: { in: STATUSES }
+
+  validate :arquivo_deve_ser_pdf
+
+  private
+
+  def arquivo_deve_ser_pdf
+    return unless arquivo.attached?
+    errors.add(:arquivo, "deve ser um PDF") unless arquivo.content_type == "application/pdf"
+  end
 end
